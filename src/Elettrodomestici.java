f15abbcb0664dadb682bd49ebfb67086a12de424
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-01-24 09:51:00 +0100

    Aggiunta dell'attributo isEcosostenibile e relativi metodi get e set all'interno delle classi.

--- src/Elettrodomestici.java
+++ src/Elettrodomestici.java
@@ -1,12 +1,20 @@
 public class Elettrodomestici {
     private String grandezza;
-    public Elettrodomestici(String grandezza){
+    private boolean isEcosostenibile;
+    public Elettrodomestici(String grandezza, boolean isEcosostenibile){
         this.grandezza=grandezza;
+        this.isEcosostenibile=isEcosostenibile;
     }
     public void setGrandezza(String grandezza){
         this.grandezza=grandezza;
     }
     public String getGrandezza(){
         return grandezza;
+    }
+    public void setEcosostenibile(boolean isEcosostenibile){
+        this.isEcosostenibile=isEcosostenibile;
+    }
+    public boolean getEcosostenibile(){
+        return isEcosostenibile;
     }
 }
--- src/ProdottiAlimentari.java
+++ src/ProdottiAlimentari.java
@@ -1,12 +1,20 @@
 public class ProdottiAlimentari {
     private String deperibilita;
-    public ProdottiAlimentari(String deperibilita){
+    private boolean isEcosostenibile;
+    public ProdottiAlimentari(String deperibilita, boolean isEcosostenibile){
         this.deperibilita=deperibilita;
+        this.isEcosostenibile=isEcosostenibile;
     }
     public void setDeperibilita(String deperibilita){
         this.deperibilita=deperibilita;
     }
     public String getDeperibilita(){
         return deperibilita;
+    }
+    public void setEcosostenibile(boolean isEcosostenibile){
+        this.isEcosostenibile=isEcosostenibile;
+    }
+    public boolean getEcosostenibile(){
+        return isEcosostenibile;
     }
 }
